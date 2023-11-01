3117b1a9b85a2dd984c03fb7c1138939fe9e7039
billyxwn-pixel <77774076+billyxwn-pixel@users.noreply.github.com>
2023-11-01 00:30:14 -0700

    Created login page, login identification with firebase, and 3 separate CSS files for each of the 3 pages we have

---
+++ scripts/firebaseAPI_BBY26.js
@@ -0,0 +1,19 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+var firebaseConfig = {
+
+    apiKey: "AIzaSyBgcOVFb3oQdFJVbJB96yBV-cs-bQmKriw",
+    authDomain: "comp1800-bby26-994ce.firebaseapp.com",
+    projectId: "comp1800-bby26-994ce",
+    storageBucket: "comp1800-bby26-994ce.appspot.com",
+    messagingSenderId: "215520092883",
+    appId: "1:215520092883:web:40898f56f802d072252a26"
+};
+
+//--------------------------------------------
+// initialize the Firebase app that's aboce
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore(); //creates a new database for us, "db"
