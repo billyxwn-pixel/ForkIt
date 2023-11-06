983470d3e8bb065896d565fac65865d05e2f670d
Gasd123 <gdupaya@my.bcit.ca>
2023-11-06 10:29:05 -0800

    added links to the firebase apis to index.html to allow the fake data to be added to firestore

--- scripts/index.js
+++ scripts/index.js
@@ -7,9 +7,9 @@ function writeRestaurants() {
         name: "Cafe One",
         latitude: 49.3128901,
         longitude: -126.71284,
-        recently_visited: 347,
+        recently_visited: 347,  
         details: "Experience artisanal coffee and delectable"
-        + "pastries at Cafe One. Join us in our cozy, artsy space!", 
+        + " pastries at Cafe One. Join us in our cozy, artsy space!", 
         email: "Cafe1_Official@yahoo.com",
         phone_number: "(250) 555-0199",
         last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
@@ -20,8 +20,8 @@ function writeRestaurants() {
         latitude: 50.312811,
         longitude: -127.71884,
         recently_visited: 53,
-        details: "Dine amidst lush gardens at Cafe Two." 
-        + "Enjoy farm-fresh cuisine in a serene oasis.", 
+        details: "Dine or enjoy a cup of coffe with us at Cafe Two." 
+        + " Enjoy fresh-grown coffee in a quiet environment.", 
         email: "Cafe2_Official@yahoo.com",
         phone_number: "(250) 756-0859",
         last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
@@ -32,9 +32,9 @@ function writeRestaurants() {
         latitude: 51.2884511,
         longitude: -128.8123124,
         recently_visited: 1012,
-        details: "Craving a quick, delicious burger? Visit FastFood One,"
-        + " where juicy burgers, crispy fries, and"
-        + " friendly service await!" , 
+        details: "Satisfy your taco cravings at FastFood Two! Fast," 
+        + " flavorful, and full of options, it's the go-to"
+        + " spot for Mexican-inspired fast food.", 
         email: "FastFood1_Official@yahoo.com",
         phone_number: "(250) 748-7823",
         last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
@@ -45,9 +45,9 @@ function writeRestaurants() {
         latitude: 48.3128901,
         longitude: -125.684584,
         recently_visited: 842,
-        details: "Satisfy your taco cravings at FastFood Two! Fast," 
-        + " flavorful, and full of options, it's the go-to"
-        + " spot for Mexican-inspired fast food.", 
+        details: "Craving a quick, delicious burger? Visit FastFood One,"
+        + " where juicy burgers, crispy fries, and"
+        + " friendly service await!" , 
         email: "FastFood2_Official@yahoo.com",
         phone_number: "(890) 123-4567",
         last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
@@ -59,8 +59,8 @@ function writeRestaurants() {
         longitude: -137.182184,
         recently_visited: 512,
         details: "Indulge in the epitome of fine dining at FancyRestaurant 1."
-        + " A symphony of flavors awaits in an opulent setting. Elegance"
-        + "in every bite.",
+        + " A symphony of flavors and elegance"
+        + "in every dish awaits you here.",
         email: "FancyRestaurant1_Official@yahoo.com",
         phone_number: "(234) 567-8901",
         last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
@@ -71,9 +71,9 @@ function writeRestaurants() {
         latitude: 58.5815219,
         longitude: -147.18123894,
         recently_visited: 512,
-        details: "Experience culinary artistry at FancyRestaurant 2."
+        details: "Experience culinary perfection at FancyRestaurant 2."
         + " Exquisite dishes, impeccable service, and a luxurious"
-        + " ambiance define your extraordinary dining journey.",
+        + " ambiance will meet you in this fine dining experience.",
         email: "FancyRestaurant2_Official@yahoo.com",
         phone_number: "(345) 678-9012",
         last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
