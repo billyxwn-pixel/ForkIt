0c2152fbe54dd58f1dfa27e1c8f1d045cae27d69
billyxwn-pixel <77774076+billyxwn-pixel@users.noreply.github.com>
2023-11-05 23:55:40 -0800

    made js files for loading data into firebase but has does not work

---
+++ scripts/start.js
@@ -0,0 +1,46 @@
+// Function to read the quote of the day from Firestore "quotes" collection
+// Input param is the String representing the day of the week, aka, the document name
+function readQuote(day) {
+    db.collection("quotes").doc(day)                                                      //name of the collection and documents should matach excatly with what you have in Firestore
+      .onSnapshot(dayDoc => {                                                               //arrow notation
+           console.log("current document data: " + dayDoc.data());                          //.data() returns data object
+           document.getElementById("quote-goes-here").innerHTML = dayDoc.data().quote;      //using javascript to display the data on the right place
+           
+           //Here are other ways to access key-value data fields
+           //$('#quote-goes-here').text(dayDoc.data().quote);         //using jquery object dot notation
+           //$("#quote-goes-here").text(dayDoc.data()["quote"]);      //using json object indexing
+		       //document.querySelector("#quote-goes-here").innerHTML = dayDoc.data().quote;
+      })
+}
+//readQuote("tuesday");        //calling the function
+
+function getDayOfWeek() {
+    const currentDate = new Date();
+    const dayOfWeek = currentDate.getDay(); // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
+
+    if (dayOfWeek === 0) {
+        // Sunday
+        return readQuote("Sunday");
+    } else if (dayOfWeek === 1) {
+        // Monday
+        return readQuote("Monday");
+    } else if (dayOfWeek === 2) {
+        // Tuesday
+        return readQuote("Tuesday");
+    } else if (dayOfWeek === 3) {
+        // Wednesday
+        return readQuote("Wednesday");
+    } else if (dayOfWeek === 4) {
+        // Thursday
+        return readQuote("Thursday");
+    } else if (dayOfWeek === 5) {
+        // Friday
+        return readQuote("Friday");
+    } else if (dayOfWeek === 6) {
+        // Saturday
+        return readQuote("Saturday");
+    }
+}
+
+const message = getDayOfWeek();
+console.log(message);
