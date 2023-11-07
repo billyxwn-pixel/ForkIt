ad5d7c7f9ff6c030d4c17779dc6eed18996102cc
Flanks365 <ble16@my.bcit.ca>
2023-11-06 21:04:24 -0800

    completed incorporating code for repeating cards in the index

--- scripts/search.js
+++ scripts/search.js
@@ -9,13 +9,16 @@ function displayCardsDyanmically(collection){
             var title = doc.data().name;
             var details = doc.data().recent_visit;
             var RestCode = doc.data().bgImage;
-            var resttime = doc.data().hours;
+            var resttime = doc.data().hours.fri;
+            var resthours = resttime.split(" ");
+            var am = resthours[0] + resthours[1];
+            var pm = resthours[2] + resthours[3];
             var restkeyword = doc.data().keywords;
             let newcard = cardTemplate.content.cloneNode(true);
-
+           
             newcard.querySelector('.texttitle').innerHTML = title;
-            newcard.querySelector('.card-text').innerHTML = restkeyword + "/ Attendance: " + details;
-            newcard.querySelector('.timetext').innerHTML = resttime;
+            newcard.querySelector('.card-text').innerHTML = restkeyword + " Attendance: " + details;
+            newcard.querySelector('.timetext').innerHTML = am + " - " + pm;
             newcard.querySelector('.card-img').src = RestCode;
 
             document.getElementById(collection + "-goes-here").appendChild(newcard)
