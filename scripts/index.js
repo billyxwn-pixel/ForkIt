3f487f3423e8e5e94b4c9740a95f871c1eb71c2d
billyxwn-pixel <77774076+billyxwn-pixel@users.noreply.github.com>
2023-11-14 02:32:54 -0800

    Added js function for loading fake reviews to Firebase, merged with changes and pushed

--- scripts/index.js
+++ scripts/index.js
@@ -21,9 +21,9 @@ function writeRestaurants() {
         latitude: 49.3128901,
         longitude: -126.71284,
         keywords: ["Cafe", "Coffee", "Pastry"],
-        recently_visited: 347, 
+        recently_visited: 347,
         details: "Experience artisanal coffee and delectable"
-        + " pastries at Cafe One. Join us in our cozy, artsy space!",
+            + " pastries at Cafe One. Join us in our cozy, artsy space!",
         stars: 4.0,
         email: "Cafe1_Official@yahoo.com",
         phone_number: "(250) 555-0199",
@@ -33,20 +33,20 @@ function writeRestaurants() {
         code: "CAFE2",
         name: "Cafe Two",
         hours: {
-             "mon": "9:00 AM 10:00 PM" ,
-             "tue": "9:00 AM 10:00 PM" ,
-             "wed": "9:00 AM 10:00 PM" ,
-             "thu": "9:00 AM 10:00 PM" ,
-             "fri": "9:00 AM 10:00 PM" ,
-             "sat": "10:00 AM 10:00 PM" ,
-             "sun": "10:00 AM 10:00 PM" 
+            "mon": "9:00 AM 10:00 PM",
+            "tue": "9:00 AM 10:00 PM",
+            "wed": "9:00 AM 10:00 PM",
+            "thu": "9:00 AM 10:00 PM",
+            "fri": "9:00 AM 10:00 PM",
+            "sat": "10:00 AM 10:00 PM",
+            "sun": "10:00 AM 10:00 PM"
         },
         latitude: 50.312811,
         longitude: -127.71884,
         keywords: ["Cafe", "Coffee", "Aesthetic"],
         recently_visited: 53,
-        details: "Dine or enjoy a cup of coffe with us at Cafe Two." 
-        + " Enjoy fresh-grown coffee in a quiet environment.", 
+        details: "Dine or enjoy a cup of coffe with us at Cafe Two."
+            + " Enjoy fresh-grown coffee in a quiet environment.",
         stars: 4.3,
         email: "Cafe2_Official@yahoo.com",
         phone_number: "(250) 756-0859",
@@ -56,21 +56,21 @@ function writeRestaurants() {
         code: "FF1",
         name: "FastFood One",
         hours: {
-             "mon": "9:00 AM 8:00 PM" ,
-             "tue": "9:00 AM 8:00 PM" ,
-             "wed": "9:00 AM 8:00 PM" ,
-             "thu": "9:00 AM 8:00 PM" ,
-             "fri": "9:00 AM 8:00 PM" ,
-             "sat": "9:00 AM 8:00 PM" ,
-             "sun": "9:00 AM 8:00 PM" 
+            "mon": "9:00 AM 8:00 PM",
+            "tue": "9:00 AM 8:00 PM",
+            "wed": "9:00 AM 8:00 PM",
+            "thu": "9:00 AM 8:00 PM",
+            "fri": "9:00 AM 8:00 PM",
+            "sat": "9:00 AM 8:00 PM",
+            "sun": "9:00 AM 8:00 PM"
         },
         latitude: 51.2884511,
         longitude: -128.8123124,
         keywords: ["Fastfood", "Taco", "Mexican"],
         recently_visited: 1012,
-        details: "Satisfy your taco cravings at FastFood Two! Fast," 
-        + " flavorful, and full of options, it's the go-to"
-        + " spot for Mexican-inspired fast food.", 
+        details: "Satisfy your taco cravings at FastFood Two! Fast,"
+            + " flavorful, and full of options, it's the go-to"
+            + " spot for Mexican-inspired fast food.",
         stars: 3.7,
         email: "FastFood1_Official@yahoo.com",
         phone_number: "(250) 748-7823",
@@ -80,21 +80,21 @@ function writeRestaurants() {
         code: "FF2",
         name: "FastFood Two",
         hours: {
-             "mon": "12:00 AM 12:00 AM" ,
-             "tue": "12:00 AM 12:00 AM" ,
-             "wed": "12:00 AM 12:00 AM" ,
-             "thu": "12:00 AM 12:00 AM" ,
-             "fri": "12:00 AM 12:00 AM" ,
-             "sat": "9:00 AM 9:00 PM" ,
-             "sun": "9:00 AM 9:00 PM" 
+            "mon": "12:00 AM 12:00 AM",
+            "tue": "12:00 AM 12:00 AM",
+            "wed": "12:00 AM 12:00 AM",
+            "thu": "12:00 AM 12:00 AM",
+            "fri": "12:00 AM 12:00 AM",
+            "sat": "9:00 AM 9:00 PM",
+            "sun": "9:00 AM 9:00 PM"
         },
         latitude: 48.3128901,
         longitude: -125.684584,
         keywords: ["Fastfood", "Burger", "Fries"],
         recently_visited: 842,
         details: "Craving a quick, delicious burger? Visit FastFood One,"
-        + " where juicy burgers, crispy fries, and"
-        + " friendly service await!" , 
+            + " where juicy burgers, crispy fries, and"
+            + " friendly service await!",
         stars: 4.0,
         email: "FastFood2_Official@yahoo.com",
         phone_number: "(890) 123-4567",
@@ -104,21 +104,21 @@ function writeRestaurants() {
         code: "FR1",
         name: "FancyRestaurant 1",
         hours: {
-             "mon": "11:00 PM 11:00 PM" ,
-             "tue": "11:00 PM 11:00 PM" ,
-             "wed": "11:00 PM 11:00 PM" ,
-             "thu": "11:00 PM 11:00 PM" ,
-             "fri": "11:00 PM 11:00 PM" ,
-             "sat": "11:00 PM 11:00 PM" ,
-             "sun": "11:00 PM 11:00 PM" 
+            "mon": "11:00 PM 11:00 PM",
+            "tue": "11:00 PM 11:00 PM",
+            "wed": "11:00 PM 11:00 PM",
+            "thu": "11:00 PM 11:00 PM",
+            "fri": "11:00 PM 11:00 PM",
+            "sat": "11:00 PM 11:00 PM",
+            "sun": "11:00 PM 11:00 PM"
         },
         latitude: 55.78511,
         longitude: -137.182184,
         keywords: ["Fancy", "Fine Dining", "Elegant"],
         recently_visited: 512,
         details: "Indulge in the epitome of fine dining at FancyRestaurant 1."
-        + " A symphony of flavors and elegance"
-        + "in every dish awaits you here.",
+            + " A symphony of flavors and elegance"
+            + "in every dish awaits you here.",
         stars: 4.5,
         email: "FancyRestaurant1_Official@yahoo.com",
         phone_number: "(234) 567-8901",
@@ -128,25 +128,49 @@ function writeRestaurants() {
         code: "FR2",
         name: "FancyRestaurant 2",
         hours: {
-             "mon": "11:00 PM 10:00 PM" ,
-             "tue": "11:00 PM 10:00 PM" ,
-             "wed": "11:00 PM 10:00 PM" ,
-             "thu": "11:00 PM 10:00 PM" ,
-             "fri": "11:00 PM 10:00 PM" ,
-             "sat": "11:00 PM 10:00 PM" ,
-             "sun": "11:00 PM 10:00 PM" 
+            "mon": "11:00 PM 10:00 PM",
+            "tue": "11:00 PM 10:00 PM",
+            "wed": "11:00 PM 10:00 PM",
+            "thu": "11:00 PM 10:00 PM",
+            "fri": "11:00 PM 10:00 PM",
+            "sat": "11:00 PM 10:00 PM",
+            "sun": "11:00 PM 10:00 PM"
         },
         latitude: 58.5815219,
         longitude: -147.18123894,
         keywords: ["Fancy", "Luxurious", "Exquisite"],
         recently_visited: 512,
         details: "Experience culinary perfection at FancyRestaurant 2."
-        + " Exquisite dishes, impeccable service, and a luxurious"
-        + " ambiance will meet you in this fine dining experience.",
+            + " Exquisite dishes, impeccable service, and a luxurious"
+            + " ambiance will meet you in this fine dining experience.",
         stars: 3.9,
         email: "FancyRestaurant2_Official@yahoo.com",
         phone_number: "(345) 678-9012",
         last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+    });
+}
+
+function writeRestaurantsReviews() {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var restaurantReviewData = db.collection("buzz_reviews");
+
+    // keywords still need to be added
+
+    restaurantReviewData.add({
+        code: "BUZZ_1",
+        name: "Seoul Eats",
+        hours: "succulent, perfectly grilled, and bursting with flavor. "
+            + "The delightful blend of savory and sweet notes made every bite "
+            + "an absolute delight, making it a must-visit for anyone craving "
+            + "an unforgettable culinary experience."
+    });
+
+    restaurantReviewData.add({
+        code: "BUZZ_2",
+        name: "Ramen Haven",
+        hours: "The ramen at Noodle Haven is simply extraordinary, with rich "
+            + "broth and perfectly textured noodles, creating an unparalleled "
+            + "dining experience."
     });
 }
 
@@ -156,12 +180,13 @@ var MaxPosts = 0;  //will get reassigned to say, 10
 var PostIndex = 0;
 
 //Next button event listener
-function addNextListener(){
-   // var index = MaxPost-PostIndex;
-    document.getElementById("buzz").addEventListener('click', ()=>{
-        if (PostIndex <= 5){
-        displayPostCard(AllPosts[PostIndex]);
-        PostIndex++;} else {
+function addNextListener() {
+    // var index = MaxPost-PostIndex;
+    document.getElementById("buzz").addEventListener('click', () => {
+        if (PostIndex <= 5) {
+            displayPostCard(AllPosts[PostIndex]);
+            PostIndex++;
+        } else {
             PostIndex = 0;
             displayPostCard(AllPosts[PostIndex])
         }
@@ -195,14 +220,14 @@ function displayPostCard(doc) {
 
     //clone the new card
     let newcard = document.getElementById("reviewCardTemp").content.cloneNode(true);
-    
+
     //populate with title, image
     newcard.querySelector('.RestTitle').innerHTML = title;
     newcard.querySelector('.Description').innerHTML = desc;
 
     //remove any old cards
     const element = document.getElementById("posts-go-here");
-    while (element.firstChild){
+    while (element.firstChild) {
         element.removeChild(element.firstChild);
     }
 
